d84230a87f0c0ed2c125c69e9cc061938a025539
UnthinkableR <UnthinkableRedstonebaf@Gmail.com>
2015-06-26 17:36:01 -0400

    Update TeamBal.java

--- src/be/UnthinkableR/TowerSiege/TeamBal.java
+++ src/be/UnthinkableR/TowerSiege/TeamBal.java
@@ -1,29 +1,21 @@
 package be.UnthinkableR.TowerSiege;
 
-import java.util.ArrayList;
 import java.util.Collection;
 
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
-import org.bukkit.scoreboard.Team;
 
 public class TeamBal {
 
-	public static void Balance(Collection<Player> players, Team bar, Team def){
-    	ArrayList<String> Done = new ArrayList<String>();
-    	ArrayList<Player> Name = new ArrayList<Player>();
-    	Name.addAll(players);
-			for(int i = 0; i < players.size() + 1; i++){
-				if(Done.size() == 1){
-					bar.addPlayer(Name.get(i-1));
-				}
-				if(Done.size() == 0){
-					def.addPlayer(Name.get(i-1));
-				}
-				if(i == players.size() / 2){
-					Done.add("1");
-				}
-			}
+	public static void Balance(Collection<Player> players){
+    	new Team("Barbarians");
+    	new Team("Defenders");
+    	
+    	int i = 1;
+    	for(Player p : players){
+    		if(i > Team.getAllTeams().size())
+    			i = 0;
+    		Team.getTeam(Team.getAllTeams().get(i).getName()).add(p);
+    	}
 	}
 	
 }
